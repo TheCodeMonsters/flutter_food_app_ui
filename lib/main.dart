7130dc99d5d8aa9caf95a000b54263089ef572fd
Manuel <carlosd99m@gmail.com>
2021-03-12 16:35:53 -0600

    food app ui - finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,240 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Food App',
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  HomepageState createState() => new HomepageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class HomepageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.lightGreen,
+      body: ListView(
+        padding: EdgeInsets.only(top: 20.0),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.arrow_back_ios),
+                onPressed: () {},
+                color: Colors.white,
+              ),
+              Container(
+                width: 125.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.filter_list),
+                      onPressed: () {},
+                      color: Colors.white,
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.menu),
+                      onPressed: () {},
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 25.0,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 40.0),
+            child: Row(
+              children: [
+                Text(
+                  'Healty ',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Food',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                    fontFamily: 'Montserrat',
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height - 85.0,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(75.0),
+                  topLeft: Radius.circular(75.0)),
             ),
-          ],
-        ),
+            child: ListView(
+              padding: EdgeInsets.only(left: 24.0, right: 24.0),
+              shrinkWrap: true,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 40.0),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height - 300,
+                  child: ListView(
+                    children: [
+                      _buildFoodItem('assets/board1.png', 'Salmon', '\$49.54'),
+                      _buildFoodItem('assets/board2.png', 'Salmon', '\$55.54'),
+                      _buildFoodItem('assets/board3.png', 'Salmon', '\$20.54'),
+                      _buildFoodItem('assets/board4.png', 'Salmon', '\$15.54'),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          border: Border.all(
+                              color: Colors.grey,
+                              width: 1.0,
+                              style: BorderStyle.solid)),
+                      child: Center(
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.black,
+                          size: 15.0,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          border: Border.all(
+                              color: Colors.grey,
+                              width: 1.0,
+                              style: BorderStyle.solid)),
+                      child: Center(
+                        child: Icon(
+                          Icons.shopping_basket,
+                          color: Colors.black,
+                          size: 15.0,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 120.0,
+                      decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(10.0),
+                          border: Border.all(
+                              width: 1.0,
+                              color: Color(0xFF1C1428),
+                              style: BorderStyle.solid)),
+                      child: Center(
+                        child: Text(
+                          'CheckOut',
+                          style: TextStyle(
+                              fontSize: 15.0,
+                              color: Colors.white,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+Widget _buildFoodItem(String imgPath, String FoodName, String price) {
+  return Padding(
+    padding: EdgeInsets.only(left: 14.0, top: 10.0),
+    child: InkWell(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            child: Row(
+              children: <Widget>[
+                Hero(
+                  tag: imgPath,
+                  child: Image(
+                    image: AssetImage(imgPath),
+                    fit: BoxFit.cover,
+                    height: 75.0,
+                    width: 75.0,
+                  ),
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      FoodName,
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25.0,
+                          color: Colors.black),
+                    ),
+                    Text(
+                      price,
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Colors.black),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+            color: Colors.black,
+          )
+        ],
+      ),
+    ),
+  );
 }
